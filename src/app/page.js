9be9497c9bc742f340adf33d18775e82582eb2c6
benjaminli7 <nicky01.bl@gmail.com>
2025-06-22 23:20:26 +0200

    change link name

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <Link href="/graph-1" target="_blank" className="font-bold text-blue-500">Graph 1</Link>
-      <Link href="/graph-2" target="_blank" className="font-bold text-blue-500">Graph 2</Link>
+      <Link href="/graph-1" target="_blank" className="font-bold text-blue-500">Graph with Flourish</Link>
+      <Link href="/graph-2" target="_blank" className="font-bold text-blue-500">Graph with Datawrapper</Link>
     </div>
   );
 }
